ab1ec632000e024efa32efb374cc48d16dc5a30b
Evert van Nieuwenburg <evert.v.nieuwenburg@gmail.com>
2022-08-17 10:30:49 +0200

    First version of training code

---
+++ train.py
@@ -0,0 +1,15 @@
+import numpy as np
+import os
+
+from FoxHoleEnvironment import FoxHole
+from stable_baselines3 import PPO
+
+env = FoxHole(5,10)
+
+os.makedirs("./logs/", exist_ok=True)
+model = PPO("MlpPolicy", env, verbose=1, tensorboard_log="./logs/")
+
+print("Training")
+model.learn(total_timesteps=1000)#, reset_num_timesteps=False)
+
+model.save("ppo_foxhole")
